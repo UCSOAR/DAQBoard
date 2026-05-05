a51a13891e6f7761749c786ee91602c75b8698dc
Andrey D <andinfbi@gmail.com>
2026-05-04 22:15:31 -0600

    Pull up for airbrakes, windowed stats, burnout detection...

--- Components/main_system.cpp
+++ Components/main_system.cpp
@@ -43,7 +43,7 @@ void run_main() {
 	CubeTask::Inst().InitTask();
 	DebugTask::Inst().InitTask();
 	FlashTask::Inst().InitTask();
-	PollingTask::Inst().InitTask();
+	// PollingTask::Inst().InitTask();
 	FilterTask::Inst().InitTask();
 	StartRunEverestInjection();
 
